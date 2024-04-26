e4488e599c8112b013486a9641e955369aa7a9ec
Marius Øgård <mo@captiva.no>
2024-04-26 09:26:05 +0200

    Clean up names

--- example.py
+++ example.py
@@ -13,27 +13,27 @@ from tyde3pub import tyde3pub
 # ]
 
 
-tydeclient = tyde3pub.TydeClient({"username": "devuser@broentech.no", "password": "testtest"})
+tyde_client = tyde3pub.TydeClient({"username": "devuser@broentech.no", "password": "testtest"})
 
-tydeclient.get_upstream_status()
-# tydeclient.print_role_info()
+tyde_client.get_upstream_status()
+# tyde_client.print_role_info()
 
-my = tydeclient.list_powerplants()
+my = tyde_client.list_powerplants()
 print("My plants:", my)
 
 # Test for Hån power plant
 pp_id = "660e967dbfe96ecfe47033f6"
 
-hppinfo = tydeclient.get_powerplant_info(pp_id)
+pp_info = tyde_client.get_powerplant_info(pp_id)
 
-haveiaccess = tydeclient.has_access_to_hpp(pp_id)
+have_access = tyde_client.has_access_to_pp(pp_id)
 
-my_sensors = tydeclient.get_sensor_for_powerplants(pp_id)
+my_sensors = tyde_client.get_sensor_for_powerplants(pp_id)
 
-my_sensor_info = tydeclient.get_sensor_info("stepsolutions.Haan.t_h01_10mindata_Grd_Prod_Pwr_Avg")
+my_sensor_info = tyde_client.get_sensor_info("stepsolutions.Haan.t_h01_10mindata_Grd_Prod_Pwr_Avg")
 print("My sensors:", my_sensor_info)
 
-# portfolio_list = tydeclient.list_portfolios()
+# portfolio_list = tyde_client.list_portfolios()
 
 sensor_ids = ["stepsolutions.Haan.t_h01_10mindata_Grd_Prod_Pwr_Avg"]
 
@@ -44,13 +44,13 @@ to_time = "2024-04-02T00:00:00Z"
 # Get alarm
 
 alarm_id = "proxima.Omron-Fins-Ethernet.Madland.SCADA_Omron.Madland.MASKIN-G1.VV.HOVEDVENTIL.AAPEN_STS"
-alarms_data = tydeclient.get_alarms(alarm_id, from_time=from_time, to_time=to_time)
+alarms_data = tyde_client.get_alarms(alarm_id, from_time=from_time, to_time=to_time)
 print("Alarms data:", alarms_data)
 
-raw_sensor_data = tydeclient.get_raw_data(sensor_ids, from_time=from_time, to_time=to_time)
+raw_sensor_data = tyde_client.get_raw_data(sensor_ids, from_time=from_time, to_time=to_time)
 print("Raw sensor data:", raw_sensor_data)
 
-sensor_data = tydeclient.get_aggregated_data(sensor_ids, from_time=from_time, to_time=to_time, aggregation="HOURLY")
+sensor_data = tyde_client.get_aggregated_data(sensor_ids, from_time=from_time, to_time=to_time, aggregation="HOURLY")
 print("Aggregated sensor data", sensor_data)
 
 # Disabled converter as of now
